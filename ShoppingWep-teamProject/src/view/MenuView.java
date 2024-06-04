d9950cdc57f7f2d2770d1b24bbbb5748a0a848c1
ckehf223 <ckehf223@gmail.com>
2024-06-04 18:02:32 +0900

    no message

--- ShoppingWep-teamProject/src/view/MenuView.java
+++ ShoppingWep-teamProject/src/view/MenuView.java
@@ -112,13 +112,5 @@ public class MenuView {
 		System.out.println("╚═════════════════════════════════════════════════════════════════╝");
 		System.out.print("**번호 선택: ");
 	}
-	public static void mainMenu() {
-        System.out.println("메뉴를 선택하세요 : ");
-        System.out.print("1. 로그인 2. 회원가입 3. 종료   ");
-    }
 
-    public static void userMenu() {
-        System.out.println("메뉴를 선택하세요 : ");
-        System.out.print("1. 마이페이지 2. 상품보기 3. 장바구니 4. 결제하기 5. 로그아웃   ");
-    }
 }
