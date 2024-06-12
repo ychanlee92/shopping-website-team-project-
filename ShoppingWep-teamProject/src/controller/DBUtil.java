0823fc169c5232ddef03ba1b5a6ca73fa2a0b991
Yehchan Lee <ychanlee92@gmail.com>
2024-06-12 20:46:00 +0900

    0612 sub-meekyoung을 메인 브랜치로 병합

--- ShoppingWep-teamProject/src/controller/DBUtil.java
+++ ShoppingWep-teamProject/src/controller/DBUtil.java
@@ -17,7 +17,7 @@ public class DBUtil {
 	// 데이터베이스 연결
 	public static Connection makeConnection() throws FileNotFoundException, IOException {
 		// 오라클 데이터베이스 정보(주소, 유저, 패스워드)
-		String filePath = "D:/teamkh/ShoppingWep-teamProject/db.properties";
+		String filePath = "C:/Users/chan/OneDrive/바탕 화면/JAVA/teamprojectfancy/ShoppingWep-teamProject/db.properties";
 		Properties properties = new Properties();
 		properties.load(new FileReader(filePath));
 
